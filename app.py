a448beb6b8f6d5f048bf91fa11e1aa53a644bb77
Omid Charrakh <omid.charrakh@gmail.com>
2023-05-02 13:00:35 +0200

    Add a sample app

--- app.py
+++ app.py
@@ -1,19 +1,46 @@
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc
+from dash import html
 import plotly.express as px
-import pandas as pd
 
-df = pd.read_csv('./files/seaborn_tips.csv')
+
+# create a sample scatter plot
+df = px.data.iris()
+fig = px.scatter(df, x="sepal_width", y="sepal_length")
 
 app = dash.Dash(__name__)
 
-app.layout = html.Div(children=[
-    html.H1('My Plotly Dash App'),
-    dcc.Graph(
-        figure=px.scatter(df, x='total_bill', y='tip')
-    )
+
+# Define scatter plot div
+scatter_plot_div = dcc.Graph(
+    id="scatter_plot",
+    figure=fig,
+    style={"height": "100%", "width": "100%"}
+)
+
+
+# Define app layout
+app.layout = html.Div([
+    html.H1("My Landing Page"),
+    html.P("Welcome to my landing page! Here's a scatter plot of some data:"),
+    scatter_plot_div,
+    html.Div([
+        html.H2("My Other Projects"),
+        html.Ul([
+            html.Li([
+                html.A("Quantum CGNN", href="https://github.com/OmidCharrakh/Quantum_CGNN")
+            ]),
+            html.Li([
+                html.A("Quantum RCC", href="https://github.com/OmidCharrakh/Quantum_RCC")
+            ]),
+            html.Li([
+                html.A("Wildlife ML", href="https://github.com/slds-lmu/wildlife-ml")
+            ])
+        ])
+    ])
 ])
 
+
 if __name__ == '__main__':
+    #app.run_server(host='127.0.0.1', port=8050, debug=True)
     app.run_server(debug=True)
